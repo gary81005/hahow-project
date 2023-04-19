87363299be55872f23cdd776c9d93d51aee1d3a6
Gary Lin (林柏岑) <garylin@ns-guard.com>
2023-04-19 12:05:47 +0800

    fix(abilities-setting): add snackbar tips to inform user

--- src/components/heroes/AbilitiesSetting.tsx
+++ src/components/heroes/AbilitiesSetting.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
+import { Alert, Box, Button, IconButton, Snackbar, Typography } from '@mui/material';
 import { AddOutlined, RemoveOutlined } from '@mui/icons-material';
 import { styled } from '@mui/system';
 import { Abilities } from '../../services/types';
@@ -40,9 +40,14 @@ const AbilitiesSetting = ({ heroId, abilities }: { heroId: string; abilities: Ab
   const [titles, setTitles] = useState<string[]>([]);
   const [values, setValues] = useState<Abilities | null>(null);
   const [remain, setRemain] = useState(0);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [status, setStatus] = useState<'warning' | 'success' | 'error'>('warning');
+  const [info, setInfo] = useState<string | null>(null);
+  const [hasChange, setHasChage] = useState(false);
 
   const handleAdd = (type: string) => {
     setRemain(remain - 1);
+    setHasChage(true);
     setValues((prev) => ({
       ...prev,
       [type]: values ? values?.[type] + 1 : 0,
@@ -51,6 +56,7 @@ const AbilitiesSetting = ({ heroId, abilities }: { heroId: string; abilities: Ab
 
   const handleMinus = (type: string) => {
     setRemain(remain + 1);
+    setHasChage(true);
     setValues((prev) => ({
       ...prev,
       [type]: values ? values?.[type] - 1 : 0,
@@ -58,53 +64,90 @@ const AbilitiesSetting = ({ heroId, abilities }: { heroId: string; abilities: Ab
   };
 
   const handleSave = () => {
-    if (values) {
-      updateHeroProfile({ heroId, abilities: values });
+    if (!hasChange) {
+      setInfo('請先調整能力值');
+      setStatus('warning');
+      setAlertOpen(true);
+      return;
     }
+    if (values) {
+      updateHeroProfile({ heroId, abilities: values })
+        .then(() => {
+          setStatus('success');
+          setInfo('更新成功');
+        })
+        .catch(() => {
+          setStatus('error');
+          setInfo('更新失敗');
+        })
+        .finally(() => {
+          setAlertOpen(true);
+          setHasChage(false);
+        });
+    }
+  };
+
+  const handleClose = () => {
+    setAlertOpen(false);
   };
 
   useEffect(() => {
     const abilitiesTitle = Object.keys(abilities);
     setTitles(abilitiesTitle);
+    setHasChage(false);
+    setAlertOpen(false);
+    setStatus('warning');
     setValues(abilities);
     setRemain(0);
   }, [abilities]);
 
   return (
-    <StyledBox>
-      <Box>
-        {titles.map((opt) => (
-          <Box>
-            <Typography key={opt} gutterBottom variant="h5" component="div">
-              {opt.toUpperCase()}
-            </Typography>
-            <IconButton size="small" disabled={remain === 0} onClick={() => handleAdd(opt)}>
-              <AddOutlined />
-            </IconButton>
-            {values ? (
-              <Typography variant="body1" component="div">
-                {values[opt]}
+    <>
+      <StyledBox>
+        <Box>
+          {titles.map((opt) => (
+            <Box>
+              <Typography key={opt} gutterBottom variant="h5" component="div">
+                {opt.toUpperCase()}
               </Typography>
-            ) : (
-              0
-            )}
-            <IconButton
-              size="small"
-              disabled={!values || values[opt] === 0}
-              onClick={() => handleMinus(opt)}
-            >
-              <RemoveOutlined />
-            </IconButton>
-          </Box>
-        ))}
-      </Box>
-      <Box>
-        <Typography>剩餘點數:{remain}</Typography>
-        <Button variant="contained" color="primary" onClick={handleSave}>
-          儲存
-        </Button>
-      </Box>
-    </StyledBox>
+              <IconButton size="small" disabled={remain === 0} onClick={() => handleAdd(opt)}>
+                <AddOutlined />
+              </IconButton>
+              {values ? (
+                <Typography variant="body1" component="div">
+                  {values[opt]}
+                </Typography>
+              ) : (
+                0
+              )}
+              <IconButton
+                size="small"
+                disabled={!values || values[opt] === 0}
+                onClick={() => handleMinus(opt)}
+              >
+                <RemoveOutlined />
+              </IconButton>
+            </Box>
+          ))}
+        </Box>
+        <Box>
+          <Typography>剩餘點數:{remain}</Typography>
+          <Button variant="contained" color="primary" onClick={handleSave}>
+            儲存
+          </Button>
+        </Box>
+      </StyledBox>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={alertOpen}
+        autoHideDuration={5000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity={status} sx={{ width: '100%' }}>
+          {info}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
